60de0c306b013ae7cb87442c8c1245dca4ab34ab
vladimir2809 <46346862+vladimir2809@users.noreply.github.com>
2022-10-02 16:03:47 +0500

    Add files via upload

--- chatForYour/js/script.js
+++ chatForYour/js/script.js
@@ -1,19 +1,19 @@
 const myWs = new WebSocket('ws://localhost:9000');
-
+var userListName = [];// масси вимен пользователей
+var selectHost = ''; // выбранный пользователь для отпраавки сообшений
 window.onload = function () {
+    // отправлеям сообшение
     var button = document.getElementById('button');
     button.onclick=function(){
         var textarea = document.getElementById('textarea');
      
-        var text = textarea.value; // "Некоторый текст" 
+        var text = textarea.value;  
         updateChat(text,0);      
-    //    // alert(text);
-    //     var elem=createElem(text);
-    //     insertElem(elem);
-
         wsSendText(text);
+        wsSendMessage('', selectHost, text); 
 
     }
+    // вход в систему
     var buttonLogin = document.getElementById('submit');
     buttonLogin.onclick = function () {
         var divChat=document.getElementById('divChat');
@@ -22,35 +22,33 @@ window.onload = function () {
         divReg.style.display = 'none';
         var login=document.getElementById('login').value;
         wsSendLogin(login);
+
         
     }
-    //setInterval(function () {
-    //    addUser('efrika');
-    //}, 1000);
-    //myWs = new WebSocket('ws://localhost:9000');
     // обработчик проинформирует в консоль когда соединение установится
     myWs.onopen = function () {
         console.log('подключился');
     };
     // обработчик сообщений от сервера
     myWs.onmessage = function (message) {
-       // console.log('Message: %s', message.data);
         console.log(message.data);
         var jsonMessage = JSON.parse(message.data);
         switch (jsonMessage.action)
         {
-            case 'TEXT': updateChat(jsonMessage.text, 1); break;
-            case 'USER':
+            case 'TEXT': updateChat(jsonMessage.text, 1); break;// пришло сообшение
+            case 'USER':// пришел список пользователей
                 {
                     var element = document.getElementById("divUserList");
                     while (element.firstChild) {
                         element.removeChild(element.firstChild);
                     }
-                    //    addUser(jsonMessage.text); break;
-                }
-                for (let i = 0; i < jsonMessage.loginArr.length;i++)
-                {
-                    addUser(jsonMessage.loginArr[i]); 
+                    userListName = [];
+
+                    for (let i = 0; i < jsonMessage.loginArr.length; i++) {
+                        addUser(jsonMessage.loginArr[i]);
+
+                    }
+                   
                 }
             break;
         }
@@ -62,6 +60,9 @@ window.onload = function () {
     function wsSendText(value) {
         myWs.send(JSON.stringify({action: 'TEXT', data: value.toString()}));
     }
+    function wsSendMessage(sender,host,value) {
+        myWs.send(JSON.stringify({action: 'MESSAGE',sender:sender,host:host ,data: value.toString()}));
+    }
     function wsSendLogin(value) {
         myWs.send(JSON.stringify({action: 'LOGIN', data: value.toString()}));
     }
@@ -70,7 +71,23 @@ window.onload = function () {
         myWs.send(JSON.stringify({action: 'PING'}));
     }
 }
-function createElem(text,receive=0,className='')
+// постоянный цикл 
+setInterval(function () {
+    document.querySelectorAll('.divUser').forEach(function (elem){
+
+            if ('<p>'+selectHost+'</p>' == elem.innerHTML)
+            {
+                elem.style.backgroundColor = 'rgb(255,128,0)';
+            }
+            else
+            {
+                elem.style.backgroundColor = 'rgb(0,255,0)';
+            }
+        
+       
+    });
+},100);
+function createElem(text,receive=0,className='')// создать елемент
 {
     let div = document.createElement('div');
                   //"message-one accepted"
@@ -87,7 +104,7 @@ function createElem(text,receive=0,className='')
     div.innerHTML = "<p>"+text+"</p>";
     return div;
 }
-function insertElem(elem,className='')
+function insertElem(elem,className='')// вставить элемент
 {
     if (className == '') className = 'message-box';
     var divBox=document.getElementById(className);
@@ -95,14 +112,19 @@ function insertElem(elem,className='')
 }
 function updateChat(text,receive)
 {
-    //var text = textarea.value; // "Некоторый текст"       
-    // alert(text);
     var elem=createElem(text,receive);
     insertElem(elem);
-    //wsSendText(text);
 }
-function addUser(text)
+function addUser(text)// добавить пользователя
 {
     var elem = createElem(text, 0, 'divUser');
+    userListName.push(text);
+    elem.id = text;
     insertElem(elem, 'divUserList');
+    document.querySelectorAll('.divUser').forEach(function (elem){
+        elem.addEventListener('click', function () { 
+            selectHost = elem.id;
+            console.log('выбран пользователь '+selectHost);
+        });
+    });
 }
